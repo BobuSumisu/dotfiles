6b6f5b145ace6eb5349b2a6cd6962a07d03a53ab
Øyvind Ingvaldsen <oyvind.ingvaldsen@gmail.com>
2017-11-20 13:52:37 +0100

    bar

---
+++ .bin/unjobb.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+PRIMARY="eDP-1"
+SECONDARY="DP-1-1"
+
+function main() {
+
+    # Ensure wanted output is connected.
+    xrandr | grep "$PRIMARY" | grep "connected" > /dev/null || {
+        echo "jobb.sh: output '$PRIMARY' is not connected"
+        exit 1
+    }
+
+    # Also ensure secondary output is connected.
+    xrandr | grep "$SECONDARY" | grep "connected" > /dev/null || {
+        echo "jobb.sh: output '$SECONDARY' is not connected"
+        exit 1
+    }
+
+    # Check if wanted primary output already is primary.
+    xrandr | grep "$PRIMARY" | grep "primary" > /dev/null && {
+        echo "jobb.sh: output '$PRIMARY' is already primary output"
+        exit 1
+    }
+
+    # Turn primary off and then on again (because of docking?).
+    # And turn off secondary output.
+    xrandr --output $PRIMARY --off
+    xrandr --output $PRIMARY --auto --primary
+    xrandr --output $SECONDARY --off
+
+}
+
+main $@
+
+unset main
